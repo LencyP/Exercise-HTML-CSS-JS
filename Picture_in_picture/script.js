60b18d5e4e8b14d4b8c637020223f4335150acc5
LenkaPuf <69017101+LencyP@users.noreply.github.com>
2021-03-23 21:25:58 +0100

    Update script.js

--- Picture_in_picture/script.js
+++ Picture_in_picture/script.js
@@ -2,7 +2,6 @@ const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 //  Prompt to select media stream, pass to video element, then play
-
 async function selectMediaStream() {
     try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
